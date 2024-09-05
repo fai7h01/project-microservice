fa8b3033265ecfa27d8472be5011ee90fdd4d750
fai7h01 <l.varsimashvil10@gmail.com>
2024-09-05 22:07:58 +0400

    project setup

--- src/main/java/com/cydeo/ProjectMicroserviceApplication.java
+++ src/main/java/com/cydeo/ProjectMicroserviceApplication.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectMicroserviceApplication {
@@ -10,4 +12,8 @@ public class ProjectMicroserviceApplication {
         SpringApplication.run(ProjectMicroserviceApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
 }
